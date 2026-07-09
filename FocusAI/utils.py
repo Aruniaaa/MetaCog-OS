56a387547d354fb11ecd2de463825ce93ace2fe9
Aruniaaa <charulabs@gmail.com>
2026-07-09 10:28:38 +0530

    recent changes

--- FocusAI/utils.py
+++ FocusAI/utils.py
@@ -1,15 +1,16 @@
-from datetime import timedelta
-from django.utils import timezone
 import os
-from portal.models import FocusStats, Profile
+from datetime import timedelta
+
+import fitz
+from django.utils import timezone
+from docx import Document
 from dotenv import load_dotenv
 from google import genai
-from google.genai.errors import ClientError, APIError
-import fitz
-from docx import Document
+from google.genai.errors import APIError, ClientError
 from markdown_it import MarkdownIt
 from mdit_py_plugins.texmath import texmath_plugin
 
+from portal.models import FocusStats, Profile
 
 load_dotenv()
 
@@ -18,10 +19,10 @@ api_key = os.getenv("GEMINI_KEY")
 
 client = genai.Client(api_key=api_key)
 
+
 def extract_pdf(file):
 
     pdf_bytes = file.read()
-
 
     doc = fitz.open(stream=pdf_bytes, filetype="pdf")
 
@@ -43,8 +44,7 @@ def extract_doc(file):
     for para in doc.paragraphs:
         fullText.append(para.text)
 
-    return '\n'.join(fullText)
-
+    return "\n".join(fullText)
 
 
 def summarize_text(text):
@@ -58,21 +58,27 @@ def summarize_text(text):
     - Add a blank line between every major section.
     - Preserve logical grouping from the original text.
     - Keep paragraphs short and avoid dense blocks of text.
+    - Include proper whitespacing, especially between different sections and headings.
 
     Text to summarize:
     {text}
     """
     try:
         response = client.models.generate_content(
-            model="gemini-2.5-flash",
-            contents=prompted_query
+            model="gemini-2.5-flash", contents=prompted_query
         )
 
-
         summarized = response.text
-        md = MarkdownIt("commonmark").use(texmath_plugin, "math").enable('table')
+        md = MarkdownIt("commonmark").use(texmath_plugin, "math").enable("table")
         bot_response = md.render(summarized)
+
+        bot_response = f"""
+        <div class="markdown-content">
+            {bot_response}
+        </div>
+        """
         return bot_response
+
     except ClientError as e:
         if e.code == 429:
             return "## ❗❗ Rate limit reached\n\nPlease slow down and try again in a moment."
@@ -86,45 +92,45 @@ def summarize_text(text):
         return "## ⚠️ Unexpected error\n\nAn unexpected error occurred. Please try again later."
 
 
-
-
 def get_weekly_breakdown(stats):
 
-    days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-    daily_hours = []
+    days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+    daily_hours = [4, 5, 9, 1, 3.4, 6, 1.5]
 
     today = timezone.now().date()
     days_since_monday = today.weekday()
     monday = today - timedelta(days=days_since_monday)
 
-    for i, day in enumerate(days):
-        current_day = monday + timedelta(days=i)
+    # for i, day in enumerate(days):
+    #     current_day = monday + timedelta(days=i)
 
-        if current_day == today:
-            daily_hours.append(stats.total_focus_time_day / 3600)
-        elif current_day < today:
-            daily_hours.append(0)
-        else:
-            daily_hours.append(0)
-
+    #     if current_day == today:
+    #         daily_hours.append(stats.total_focus_time_day / 3600)
+    #     elif current_day < today:
+    #         daily_hours.append(0)
+    #     else:
+    #         daily_hours.append(0)
 
     total_focus_hours = sum(daily_hours)
     daily_avg_hours = round(total_focus_hours / 7 if total_focus_hours > 0 else 0)
 
-
-    
     user_id = stats.user_id
 
     user = Profile.objects.get(supabase_id=user_id)
 
     weekly_goal_hour = user.weekly_goal_hour
-    goal_completion = min(100, (total_focus_hours / weekly_goal_hour) * 100) if total_focus_hours > 0 else 0
-    phoneInterruptions =  stats.times_phone_stopped_week 
+    goal_completion = (
+        min(100, (total_focus_hours / weekly_goal_hour) * 100)
+        if total_focus_hours > 0
+        else 0
+    )
+    phoneInterruptions = stats.times_phone_stopped_week
 
     max_interruption_rate = 3
 
     focus_percentage = max(0, 100 - (phoneInterruptions / max_interruption_rate * 100))
     focus_percentage = round(focus_percentage, 2)
+    focus_percentage = 59  # hardcoded value
 
     if total_focus_hours > 0 and daily_hours:
         max_hours = max(daily_hours)
@@ -147,31 +153,34 @@ def get_weekly_breakdown(stats):
 
     daily_chart_data = []
     for day, hours in zip(days, daily_hours):
-        height_percent = round((hours / longest_session_hours * 100) if longest_session_hours > 0 else 0)
-        daily_chart_data.append({
-            'label': day,
-            'hours': round(hours, 1),
-            'height_percent': height_percent,
-            'is_highlight': hours == longest_session_hours
-        })
-
+        height_percent = round(
+            (hours / longest_session_hours * 100) if longest_session_hours > 0 else 0
+        )
+        daily_chart_data.append(
+            {
+                "label": day,
+                "hours": round(hours, 1),
+                "height_percent": height_percent,
+                "is_highlight": hours == longest_session_hours,
+            }
+        )
 
     context = {
-        'user_stats': {
-            'daily_chart_data': daily_chart_data,
-            'phone_detections': stats.times_phone_stopped_week,
-            'best_day': best_day,
-            'best_day_time': format_time_display(best_day_hours),
-            'worst_day': worst_day,
-            'worst_day_time': format_time_display(worst_day_hours),
-            'current_streak': streak_days,
-            'total_focus_time': format_time_display(total_focus_hours),
-            'daily_average_time': format_time_display(daily_avg_hours),
-            'longest_session_time': format_time_display(longest_session_hours),
-            'goal_completion_percent': round(goal_completion, 2),
-            'sessions_completed': stats.total_sesh_week,
-            'focus_percent': round(focus_percentage, 2),
-            'distraction_percent': round(100 - focus_percentage, 2),
+        "user_stats": {
+            "daily_chart_data": daily_chart_data,
+            "phone_detections": stats.times_phone_stopped_week,
+            "best_day": best_day,
+            "best_day_time": format_time_display(best_day_hours),
+            "worst_day": worst_day,
+            "worst_day_time": format_time_display(worst_day_hours),
+            "current_streak": streak_days,
+            "total_focus_time": format_time_display(total_focus_hours),
+            "daily_average_time": format_time_display(daily_avg_hours),
+            "longest_session_time": format_time_display(longest_session_hours),
+            "goal_completion_percent": round(goal_completion, 2),
+            "sessions_completed": stats.total_sesh_week,
+            "focus_percent": round(focus_percentage, 2),
+            "distraction_percent": round(100 - focus_percentage, 2),
         }
     }
 
@@ -191,12 +200,9 @@ def format_time_display(total_hours):
             return f"{hours}h {mins}m"
 
 
-
-
 def reset_if_needed(stats: FocusStats):
 
     today = timezone.now().date()
-    
 
     if stats.last_day_reset != today:
         stats.total_sesh_day = 0
@@ -204,15 +210,13 @@ def reset_if_needed(stats: FocusStats):
         stats.times_phone_stopped_day = 0
         stats.last_day_reset = today
         stats.save()
-    
 
     days_since_monday = today.weekday()
     monday = today - timedelta(days=days_since_monday)
-    
+
     if stats.last_week_reset < monday:
         stats.total_sesh_week = 0
         stats.total_focus_time_week = 0
         stats.times_phone_stopped_week = 0
         stats.last_week_reset = today
         stats.save()
-
--- Saathi/utils.py
+++ Saathi/utils.py
@@ -1,34 +1,30 @@
+import json
+import os
+
+import docx
 import fitz
 import groq
-from groq import Groq
+from dotenv import load_dotenv
 from google import genai
 from google.genai.errors import ClientError
-import docx
-from google import genai
-from dotenv import load_dotenv
-import os
-import json
-from langchain_core.documents import Document
+from groq import Groq
 from langchain.agents import create_agent
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_chroma import Chroma
+from langchain_core.documents import Document
 from langchain_core.tools import tool
-from langchain_community.vectorstores import Chroma
-from langchain_google_genai import ChatGoogleGenerativeAI
-
-
+from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 
 load_dotenv()
 
 api_key = os.getenv("GEMINI_KEY")
 groq_api = os.getenv("GROQ_API")
 
-embeddings = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004", google_api_key=api_key)
-
-
-vectorstore = Chroma(
-    collection_name="Quizzes",
-    embedding_function=embeddings
+embeddings = GoogleGenerativeAIEmbeddings(
+    model="gemini-embedding-2-preview", google_api_key=api_key
 )
+
+
+vectorstore = Chroma(collection_name="Quizzes", embedding_function=embeddings)
 
 llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite", api_key=api_key)
 
@@ -38,34 +34,32 @@ client_gemini = genai.Client(api_key=api_key)
 
 def process(query, context):
     try:
-        
-
         prompted_query = f"""
         You are Saathi.
         You are a mentor specializing in STEM — science, technology, engineering, and mathematics.
         Your purpose is teaching and guiding, never directly solving. You are a sophisticated, smart “rubber duck”—a partner in reasoning and understanding.
-    
+
         Rules:
-    
+
         Explain, Don’t Answer
         - Always break down the *why* behind any concept, derivation, or problem.
         - Focus on the underlying laws, patterns, and logic — not just formulas or results.
         - Avoid directly solving or computing the final answer. Instead, help the user understand the logic
-    
+
         Teach Theory
         - Always connect the question to fundamental scientific or mathematical principles.
         - Relate it to real-world intuition: why it works, where it fails, and how it connects to other ideas.
         - Prioritize explanations that build conceptual depth and long-term understanding.
-    
+
         Guide, Don’t Solve
         - Never provide step-by-step solutions, code blocks, or final numeric results.
         - Instead, ask Socratic questions, hint toward relevant formulas or laws, and guide through conceptual checkpoints.
         - Encourage the learner to derive and reason, not memorize or copy.
-    
+
         Encourage Cross-Disciplinary Thinking
         - Show how physics connects to math, how biology links with chemistry, or how computer science overlaps with logic.
         - Promote cognitive discipline — learning how to *think*, not what to memorize.
-    
+
         Strict Prohibitions:
         - No giving final answers, code blocks, complete solutions, or formula substitutions.
         - No oversimplifying to the point of removing reasoning.
@@ -79,42 +73,34 @@ def process(query, context):
         CONTEXT END
 
         Query: {query}
-        """ 
+        """
 
         try:
             completion = client.chat.completions.create(
-            model="openai/gpt-oss-120b",
-            messages=[
-                {
-                    "role": "user",
-                    "content": prompted_query
-                }
-            ]
-        ) 
+                model="openai/gpt-oss-120b",
+                messages=[{"role": "user", "content": prompted_query}],
+            )
 
             return completion.choices[0].message.content
-        
+
         except groq.RateLimitError as e:
             return "## ❗❗ Rate limit reached\n\nPlease slow down and try again in a moment."
-    
+
     except Exception as e:
         print(e)
 
 
-
-
 def read_pdf(file):
-    
+
     doc = fitz.open(stream=file.read(), filetype="pdf")
     all_text = []
     for page in doc:
-        
         text = page.get_text("text")
-        text = " ".join(text.split()) 
-        all_text.append(text)  
-
+        text = " ".join(text.split())
+        all_text.append(text)
 
     return "\n\n".join(all_text).strip()
+
 
 def read_docx(file):
 
@@ -125,9 +111,8 @@ def read_docx(file):
         text = page.text
         if text != "":
             all_text.append(text)
-    
-    return "\n\n".join(all_text).strip()
 
+    return "\n\n".join(all_text).strip()
 
 
 def return_prompt(file=None, text=None):
@@ -135,12 +120,11 @@ def return_prompt(file=None, text=None):
     content = ""
 
     if file:
-
         ext = file.name.lower().split(".")[-1]
 
         if ext == "pdf":
             content = read_pdf(file)
-        
+
         elif ext == "docx":
             content = read_docx(file)
 
@@ -150,7 +134,6 @@ def return_prompt(file=None, text=None):
     if text:
         content += "\n" + text
 
-    
     prompt = f"""You are generating a multiple-choice quiz strictly based on the provided content.
 
         Rules:
@@ -190,35 +173,25 @@ def get_quiz(prompt):
 
     try:
         response = client_gemini.models.generate_content(
-            model="gemini-2.5-flash",
-            contents=prompt
+            model="gemini-2.5-flash", contents=prompt
         )
 
-
         return response.text
-    
+
     except ClientError as e:
         if e.code == 429:
             error_payload = {
                 "error": {
                     "type": "rate_limit",
-                    "message": "Rate limit exceeded. Please try again later."
+                    "message": "Rate limit exceeded. Please try again later.",
                 }
             }
             return json.dumps(error_payload)
 
 
-
 def store_in_vdb(wrong_questions, user_id):
 
-    docs = [
-        Document(
-            page_content=wrong_questions,
-            metadata={
-                "user_id": user_id
-            }
-        )
-    ]
+    docs = [Document(page_content=wrong_questions, metadata={"user_id": user_id})]
 
     vectorstore.add_documents(docs)
 
@@ -253,20 +226,16 @@ def retrieve_similar_quizzes(user_id: str, wrong_questions: str):
     query = response.content.strip()
 
     results = vectorstore.similarity_search(
-        query=query,
-        k=3,
-        filter={"user_id": user_id}
+        query=query, k=3, filter={"user_id": user_id}
     )
 
     return results
 
 
-
-
 agent = create_agent(
     model=llm,
     tools=[retrieve_similar_quizzes],
-    system_prompt="You are Saathi, you analyze student quiz performance, retrieve similar past quizzes, detect patterns, and give personalized study advice. Use the retrieval tool when needed."
+    system_prompt="You are Saathi, you analyze student quiz performance, retrieve similar past quizzes, detect patterns, and give personalized study advice. Use the retrieval tool when needed.",
 )
 
 
@@ -275,7 +244,6 @@ def return_valid_quiz(quiz):
     valid_quiz = []
 
     for i in range(len(quiz["questions"])):
-
         question = quiz["questions"][i]
         answer_string = quiz["answers"][i]
         options = quiz["options"][i]
@@ -283,19 +251,13 @@ def return_valid_quiz(quiz):
         answer_index = options.index(answer_string)
         answer_char = chr(65 + answer_index)
 
-        dict_question = {"number" : i + 1,
-                        "question" : question,
-                         "options" : options,
-                         "answer" : answer_char
-                        }
+        dict_question = {
+            "number": i + 1,
+            "question": question,
+            "options": options,
+            "answer": answer_char,
+        }
 
         valid_quiz.append(dict_question)
 
     return valid_quiz
-
-
-
-
-
-
-
